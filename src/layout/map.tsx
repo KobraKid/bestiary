ccbc8ab5414fdec88b740ee7cf82984949be0942
Michael Huyler <michaelhuyler@gmail.com>
2022-08-14 20:39:22 -0500

    Fix zooming for maps

--- src/layout/map.tsx
+++ src/layout/map.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState, WheelEvent } from 'react';
+import React, { useCallback, useEffect, useState, WheelEvent } from 'react';
 import ScrollContainer from 'react-indiana-drag-scroll';
 import { ILayoutElement } from '../interfaces/ILayout';
 import { ICollection, IEntry } from '../interfaces/IPackage';
@@ -15,15 +15,16 @@ export interface IMapProps extends ILayoutElement {
 }
 
 export const Map = (props: IMapProps) => {
-  const { type: _, data } = props;
+  const { type: _, data, value, poi, onLocationClicked } = props;
 
-  const image = getValueOrLiteral<string>(data.entry.attributes, props.value);
-
+  const image = getValueOrLiteral<string>(data.entry.attributes, value);
   if (!image) { return null; }
 
   const size = getValueOrLiteral<string>(data.entry.attributes, "!size").split(",");
-  const pointOfInterest = getValueOrLiteral<IPointOfInterestProps[] | null | undefined>(data.entry.attributes, props.poi);
+  const pointOfInterest = getValueOrLiteral<IPointOfInterestProps[] | null | undefined>(data.entry.attributes, poi);
   const [scale, setScale] = useState<number>(100);
+
+  useEffect(() => setScale(100), [image]);
 
   const onWheelZoomCallback = useCallback((e: WheelEvent<HTMLImageElement>) => {
     if (e.deltaY < 0) {
@@ -36,24 +37,31 @@ export const Map = (props: IMapProps) => {
     }
   }, []);
 
-
   return (
-    <ScrollContainer style={{ height: "calc(100vh - 72px)", width: "calc(100vw - 72px)", padding: "4px" }}>
-      <img src={window.path.join(data.pkg.metadata.path, image)} style={{ transform: `scale(${scale / 100}, ${scale / 100})` }} useMap={`#${props.value}`} onWheel={onWheelZoomCallback} />
-      {
-        pointOfInterest?.map((point, i) =>
-          <PointOfInterest
-            key={i}
-            data={data}
-            link={point.link}
-            location={point.location}
-            size={point.size}
-            scale={scale}
-            parentWidth={size[0]!}
-            parentHeight={size[1]!}
-            onLocationClicked={props.onLocationClicked} />)
-      }
-    </ScrollContainer>
+    <div onWheel={onWheelZoomCallback}>
+      <ScrollContainer
+        style={{ height: "calc(100vh - 72px)", width: "calc(100vw - 72px)", padding: "4px" }}>
+        <img
+          src={window.path.join(data.pkg.metadata.path, image)}
+          style={{
+            width: `${+(size[0]!) * scale / 100}px`,
+            height: `${+(size[1]!) * scale / 100}px`,
+          }} />
+        {
+          pointOfInterest?.map((point, i) =>
+            <PointOfInterest
+              key={i}
+              data={data}
+              link={point.link}
+              location={point.location}
+              size={point.size}
+              scale={scale}
+              parentWidth={size[0]!}
+              parentHeight={size[1]!}
+              onLocationClicked={onLocationClicked} />)
+        }
+      </ScrollContainer>
+    </div>
   );
 }
 
@@ -75,6 +83,7 @@ export const PointOfInterest = (props: IPointOfInterestProps) => {
 
   const location = props.location.split(",");
   const size = props.size.split(",");
+  const scale = props.scale / 100;
 
   const linkedCollection = data.pkg.collections?.find((collection: ICollection) => collection.name === props.link[0]);
   const linkedEntry = linkedCollection?.data?.find((entry: IEntry) => entry.id === props.link[1]);
@@ -88,11 +97,10 @@ export const PointOfInterest = (props: IPointOfInterestProps) => {
         className='mapPOI'
         style={{
           position: "relative",
-          left: `${location[0]}px`,
-          top: `calc(-${props.parentHeight}px + ${location[1]}px - 4px)`,
-          width: `${size[0]}px`,
-          height: `${size[1]}px`,
-          transform: `scale(${props.scale / 100}, ${props.scale / 100}) translate(calc(${location[0]}px * ${(props.scale / 100)-1}), calc((-${props.parentHeight}px + ${location[1]}px - 4px) * ${(props.scale / 100)-1}))`,
+          left: `${+(location[0]!) * scale}px`,
+          top: `calc(-${props.parentHeight}px * ${scale} + ${location[1]}px * ${scale} - 4px)`,
+          width: `${+(size[0]!) * scale}px`,
+          height: `${+(size[1]!) * scale}px`,
         }}
         onClick={() => props.onLocationClicked(linkedEntry, linkedCollection, null, data.collection)} />
     </div>
