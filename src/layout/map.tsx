67ebe031f8711d7b78215639d05276042d1b8453
Michael Huyler <michaelhuyler@gmail.com>
2022-11-20 12:36:09 -0600

    Style map, add title, initial scroll pos

--- src/layout/map.tsx
+++ src/layout/map.tsx
@@ -1,16 +1,18 @@
-import React, { useCallback, useEffect, useState, WheelEvent } from 'react';
+import React, { useCallback, useEffect, useRef, useState, WheelEvent } from 'react';
 import ScrollContainer from 'react-indiana-drag-scroll';
 import { IDataProps, ILayoutElement, ILayoutProps, ILinkableProps } from '../model/Layout';
 import { getStyle, getValueOrLiteral } from './base';
 import ICollection from '../model/Collection';
 import IEntry from '../model/Entry';
-import '../styles/collection.scss';
 import { AttributeValue } from '../model/Attribute';
+import '../styles/collection.scss';
+import '../styles/map.scss';
 
 // =============================================================================
 // | Map
 // =============================================================================
 export interface IMapLayoutProps extends ILayoutProps {
+  name: string,
   value: string,
   poi: AttributeValue[]
 }
@@ -22,19 +24,23 @@ export interface IMapProps extends ILayoutElement {
 export const Map = (props: IMapProps) => {
   const { layout, data, onLinkClicked } = props;
 
+  const name = getValueOrLiteral(data, layout.name);
   const image = getValueOrLiteral(data, layout.value);
   if (!image) { return null; }
 
   const size = (getValueOrLiteral(data, "!size") as string).split(",");
   const pointOfInterest = getValueOrLiteral(data, layout.poi) as AttributeValue[];
+
   const [scale, setScale] = useState<number>(100);
   const minScale = 10;
   const maxScale = 1000;
   const increment = 10;
+  useEffect(() => setScale(100), [image]);
 
   const style = getStyle(data, layout.style);
 
-  useEffect(() => setScale(100), [image]);
+  const container = useRef<HTMLElement>(null);
+  useEffect(() => container.current?.scrollTo(0, 0), [image]);
 
   const onWheelZoomCallback = useCallback((e: WheelEvent<HTMLImageElement>) => {
     if (e.deltaY < 0) {
@@ -48,32 +54,34 @@ export const Map = (props: IMapProps) => {
   }, []);
 
   return (
-    <div onWheel={onWheelZoomCallback}>
-      <ScrollContainer
-        style={{ height: "calc(100vh - 64px)", width: "calc(100vw - 64px)" }}>
-        <img
-          src={window.path.join(data.pkg.metadata.path, "" + image)}
-          style={{
-            width: `${+(size[0]!) * scale / 100}px`,
-            height: `${+(size[1]!) * scale / 100}px`,
-            marginLeft: `max(0px, calc(100vw - 64px - ${+(size[0]!) * scale / 100}px))`,
-            marginRight: `max(0px, calc(100vw - 64px - ${+(size[0]!) * scale / 100}px))`,
-            marginTop: `max(0px, calc(100vh - 64px - ${+(size[1]!) * scale / 100}px))`,
-            marginBottom: `max(0px, calc(100vh - 64px - ${+(size[1]!) * scale / 100}px))`,
-            ...style
-          }} />
-        {pointOfInterest && pointOfInterest?.map((point, i) =>
-          <PointOfInterest
-            key={i}
-            point={point}
-            scale={scale}
-            parentWidth={size[0]!}
-            parentHeight={size[1]!}
-            data={data}
-            onLinkClicked={onLinkClicked} />)
-        }
-      </ScrollContainer>
-    </div>
+    <React.Fragment>
+      <div className='map-title'><div>{name}</div></div>
+      <div onWheel={onWheelZoomCallback}>
+        <ScrollContainer className='map-scroll-container' innerRef={container}>
+          <img
+            src={window.path.join(data.pkg.metadata.path, "" + image)}
+            style={{
+              width: `${+(size[0]!) * scale / 100}px`,
+              height: `${+(size[1]!) * scale / 100}px`,
+              marginLeft: `max(0px, calc(100vw - 64px - ${+(size[0]!) * scale / 100}px))`,
+              marginRight: `max(0px, calc(100vw - 64px - ${+(size[0]!) * scale / 100}px))`,
+              marginTop: `max(0px, calc(100vh - 64px - ${+(size[1]!) * scale / 100}px))`,
+              marginBottom: `max(0px, calc(100vh - 64px - ${+(size[1]!) * scale / 100}px))`,
+              ...style
+            }} />
+          {pointOfInterest && pointOfInterest?.map((point, i) =>
+            <PointOfInterest
+              key={i}
+              point={point}
+              scale={scale}
+              parentWidth={size[0]!}
+              parentHeight={size[1]!}
+              data={data}
+              onLinkClicked={onLinkClicked} />)
+          }
+        </ScrollContainer>
+      </div>
+    </React.Fragment>
   );
 }
 
@@ -93,19 +101,22 @@ export const PointOfInterest = (props: IPointOfInterestProps) => {
 
   const point = props.point.toString().split('||').map(val => val.trim());
 
-  const link = (point[0] as string).split('|');
+  const link = [(point[0] as string).split('|')[0]?.substring(1), (point[0] as string).split('|')[1]];
   const location = (point[1] as string).split(',');
   const size = (point[2] as string).split(',');
   const scale = props.scale / 100;
 
-  const linkedCollection = data.pkg.collections?.find((collection: ICollection) => collection.name === link[0]?.substring(1));
+  const linkedCollection = data.pkg.collections?.find((collection: ICollection) => collection.name === link[0]);
   const linkedEntry = linkedCollection?.data?.find((entry: IEntry) => entry.id === link[1]);
 
-  if (!linkedCollection || !linkedEntry || !onLinkClicked) { return null; }
+  if (!linkedCollection || !linkedEntry || !onLinkClicked) {
+    window.electronAPI.writeError(`Could not establish POI link [${link.toString()}]:${!linkedCollection ? " Missing collection" : ""}${!linkedEntry ? " Missing entry" : ""}${!onLinkClicked ? " Missing click handler" : ""}`);
+    return null;
+  }
 
   // extra div to prevent siblings' heights from stacking
   return (
-    <div style={{ height: "0px", width: "0px" }}>
+    <div className='poi-container'>
       <div
         className='mapPOI'
         style={{
